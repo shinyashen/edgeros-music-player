804d9b58e4e6767eff20784e65272ad791a5abb5
JonnyW <1578263859@qq.com>
2023-07-06 14:30:33 +0800

    完成琴键点按

--- musicplayer-app/src/router/index.ts
+++ musicplayer-app/src/router/index.ts
@@ -1,19 +1,10 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Play from '../views/Play.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/play',
+    component: Play
   }
 ]
 
